c6a08583523f313a2ba400b1336834ba227be8ff
= <federicosuper898@gmail.com>
2021-09-30 11:44:14 +0200

    esercizio 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-maps';
+  title = 'server mappe';
+  //Aggiungiamo latitudine e longitudine di un luogo
+  center : any;
+  position : any;
+  label :string;
+  casa_mia: any;
+  casa_mia_pos: any;
+  casa_label: string;
+  constructor()
+  {
+    this.center={lat: 45.506738, lng: 9.190766};
+    this.position = this.center;
+    this.label = 'ciao';
+
+    this.casa_mia = {lat: 45.517940, lng: 9.186850};
+    this.casa_mia_pos = this.casa_mia;
+    this.casa_label = 'casa mia'
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { GoogleMapsModule } from '@angular/google-maps' // Importa questo modulo
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GoogleMapsModule // Importa questo modulo
   ],
   providers: [],
   bootstrap: [AppComponent]
