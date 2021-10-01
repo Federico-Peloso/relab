b8d6966aedd3e137182f2578883cf3e35c7efb38
= <federicosuper898@gmail.com>
2021-10-01 11:29:26 +0200

    01.1, esercizio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,11 @@ export class AppComponent {
   casa_mia: any;
   casa_mia_pos: any;
   casa_label: string;
+  circleOptions: {fillColor: string};
+  secondo_cerchio: any;
+  secondo_cerchio_pos: any;
+  secondo_cerchio_laber: string;
+  secondoCerchioOptions: {fillColor: 'blue'};
   constructor()
   {
     this.center={lat: 45.506738, lng: 9.190766};
@@ -22,7 +27,14 @@ export class AppComponent {
 
     this.casa_mia = {lat: 45.517940, lng: 9.186850};
     this.casa_mia_pos = this.casa_mia;
-    this.casa_label = 'casa mia'
+    this.casa_label = 'casa mia';
+    this.circleOptions = {fillColor: 'red'};
+
+    this.secondo_cerchio = {lat: 45.506778, lng: 9.190266}
+    this.secondo_cerchio_pos = this.secondo_cerchio
+    this.secondo_cerchio_laber = 'secondo cerchio'
+    this.secondoCerchioOptions = {fillColor: 'blue'}
+
   }
 
 }
