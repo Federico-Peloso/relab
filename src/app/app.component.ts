ce06b46b035e0860c8d92eb242daae0c29e91edd
= <federicosuper898@gmail.com>
2021-10-07 11:51:18 +0200

    esercizi fino a triangolo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,14 @@ export class AppComponent {
   secondo_cerchio_laber: string;
   secondoCerchioOptions: {fillColor: string};
 
+  markerOptions: google.maps.MarkerOptions;
+  gatto_label: string;
+
+  imgs: google.maps.MarkerOptions[];
+
   public colori:Array<string> = ['yellow', 'black', 'blue'];
+
+  vertices: google.maps.LatLngLiteral[];
 
   public cambiaColore(colore:string) {
     this.circleOptions = {fillColor: colore}
@@ -29,7 +36,7 @@ export class AppComponent {
 
   constructor()
   {
-    this.center={lat: 45.506738, lng: 9.190766};
+    this.center = {lat: 45.506738, lng: 9.190766};
     this.position = this.center;
     this.label = 'ciao';
 
@@ -42,6 +49,38 @@ export class AppComponent {
     this.secondo_cerchio_pos = this.secondo_cerchio
     this.secondo_cerchio_laber = 'secondo cerchio'
     this.secondoCerchioOptions = {fillColor: 'blue'}
+
+    let iconData : google.maps.Icon = {
+      url:'./assets/img/cat_acrobat.ico',
+      scaledSize : new google.maps.Size(60,60)
+    }
+
+    let img1 : google.maps.Icon = {
+      url: 'https://www.google.com/s2/favicons?sz=64&domain=neca.it',
+      scaledSize: new google.maps.Size(60, 60),
+      
+    }
+
+    let img2 : google.maps.Icon = {
+      url: 'https://www.google.com/s2/favicons?domain=brainly.com',
+      scaledSize: new google.maps.Size(60, 60)
+    }
+
+    let img3 : google.maps.Icon = {
+      url: 'https://www.google.com/s2/favicons?domain=quizlet.com',
+      scaledSize: new google.maps.Size(60, 60)
+    }
+  
+    this.markerOptions = {icon:iconData}
+    this.gatto_label = 'gatto'
+
+    this.imgs = [{icon:img1, position: new google.maps.LatLng(45.498878, 9.190166)}, {icon:img2, position: new google.maps.LatLng(45.503878, 9.195166)}, {icon: img3, position: new google.maps.LatLng(45.511878, 9.190466)}]
+
+    this.vertices = [
+      {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      {  lat: this.center.lat, lng: this.center.lng },
+      {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002}
+    ];
 
   }
 
